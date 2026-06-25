6eeacae733fdad06d33d61e09c8f735b39946c8b
huffman1292 <huffman1292@gmail.com>
2026-06-25 14:07:32 -0400

    Initial project setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-// MACDC Drain Map
-// Main application script
-
-console.log("MACDC Drain Map loaded.");
+fetch("assets/svg/michigan-counties.svg")
+    .then(response => response.text())
+    .then(svg => {
+        document.getElementById("map-container").innerHTML = svg;
+    })
+    .catch(error => {
+        console.error(error);
+    });
